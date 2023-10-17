ea314b1b269023bd0f1e56abb51b561f917f151b
cancarlose <carlosedlima4@gmail.com>
2023-10-17 17:45:27 -0300

    commit

---
+++ 02 FUNDAMENTOS/tipo boolean/index.js
@@ -0,0 +1,4 @@
+// Declaração de variavel boolean
+var a = false;
+console.log(a);
+console.log(typeof a);
---
+++ 02 FUNDAMENTOS/tipo boolean/index.ts
@@ -0,0 +1,5 @@
+// Declaração de variavel boolean
+let a: boolean = false
+
+console.log(a)
+console.log(typeof a)
