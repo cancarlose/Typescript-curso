9ee86d740d8cab229ab13e80ae763c494d734ea6
cancarlose <carlosedlima4@gmail.com>
2023-10-17 17:53:28 -0300

    commit

---
+++ 02 FUNDAMENTOS/tipo string/index.js
@@ -0,0 +1,9 @@
+// Declaração de Variavel 
+var firstName = 'Matheus';
+// FUncionamento JSON 
+console.log(firstName.toUpperCase());
+var lastName = 'Battisti';
+// Variavel tipada mas ainda não declarada
+var fullName;
+fullName = firstName + ' ' + lastName;
+console.log(fullName);
---
+++ 02 FUNDAMENTOS/tipo string/index.ts
@@ -0,0 +1,13 @@
+// Declaração de Variavel 
+const firstName: string = 'Matheus'
+// FUncionamento JSON 
+console.log(firstName.toUpperCase())
+
+const lastName: string = 'Battisti'
+
+// Variavel tipada mas ainda não declarada
+let fullName: string
+
+fullName = firstName + ' ' + lastName
+
+console.log(fullName)
